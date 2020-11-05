06b5c66130458e231b10352f99ece87b2205ac90
Le Anh Duc <55091357+ladcva@users.noreply.github.com>
2020-11-05 10:55:38 +0700

    config setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name = 'preprocess_kgptalkie', #this should be unique
+	include_package_data=True,
+	version = '0.1.0',
+	author = 'Le Anh Duc',
+	author_email = 'ladcva@yahoo.com',
+	description = 'Preprocessing package',
+	long_description = '',
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
